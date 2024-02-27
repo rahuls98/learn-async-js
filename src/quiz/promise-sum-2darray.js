f15eaebddadabe938fa8ee6b828673a26e3526d7
Rahul Suresh <rahs98@gmail.com>
2024-02-26 19:46:27 -0600

    Class work

--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -1,5 +1,34 @@
 const array2D = [
     [1, 2, 3],
     [4, 5, 6],
-    [7, 8, 9]
+    [7, 8, 9],
 ];
+
+function sum2DArrayConcurrent(arr) {
+    return new Promise((resolve, reject) => {
+        if (!Array.isArray(arr)) {
+            reject("BAD INPUT: Expected array as input");
+            return;
+        }
+
+        const flattenedArray = arr.flat();
+
+        Promise.all(flattenedArray.map((value) => Promise.resolve(value)))
+            .then((values) => {
+                const sum = values.reduce((acc, curr) => acc + curr, 0);
+                resolve(sum);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
+
+const sumPromise = sum2DArrayConcurrent(array2D);
+sumPromise
+    .then((result) => {
+        console.log("Sum of array2D:", result);
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
